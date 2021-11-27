e2c3758c6591c0ccd2cc296e2642bb21ec3c49fc
Gabriel Couto <Gabriel_Couto@gap.com>
2021-11-27 16:02:56 -0300

    increase coverage

--- src/shared/Text/TextComponent.test.tsx
+++ src/shared/Text/TextComponent.test.tsx
@@ -3,7 +3,27 @@ import {render} from 'utils/testUtils';
 
 import {Text} from 'shared/Text/TextComponent';
 
-test('Text component snapshot', () => {
+test('snapshot with regular fontWeight', () => {
   const {toJSON} = render(<Text color='primary'>Lato Black</Text>);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('snapshot with Thin fontWeight', () => {
+  const {toJSON} = render(<Text color='primary' fontFamily='Thin'>Lato Black</Text>);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('snapshot with Light fontWeight', () => {
+  const {toJSON} = render(<Text color='primary' fontFamily='Light'>Lato Black</Text>);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('snapshot with Bold fontWeight', () => {
+  const {toJSON} = render(<Text color='primary' fontFamily='Bold'>Lato Black</Text>);
+  expect(toJSON()).toMatchSnapshot();
+});
+
+test('snapshot with Black fontWeight', () => {
+  const {toJSON} = render(<Text color='primary' fontFamily='Black'>Lato Black</Text>);
   expect(toJSON()).toMatchSnapshot();
 });
