4eebc7b130a57ba8ce9b3c98f23a21b5eb2a4f62
Gabriel Couto <Gabriel_Couto@gap.com>
2021-11-28 16:18:56 -0300

    lint files

--- src/shared/Text/TextComponent.tsx
+++ src/shared/Text/TextComponent.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React from "react";
+import styled from "styled-components/native";
 import {
   color,
   ColorProps,
@@ -9,57 +9,63 @@ import {
   FontSizeProps,
   fontFamily,
   FontFamilyProps,
-}
-  from 'styled-system';
+} from "styled-system";
 
-const StyledText = styled.Text<ColorProps&SpaceProps&FontSizeProps&FontFamilyProps>`
+const StyledText = styled.Text<ColorProps & SpaceProps & FontSizeProps & FontFamilyProps>`
   ${color};
   ${space};
   ${fontSize}
   ${fontFamily}
 `;
 
-const buildFontWeight = (fontWeight: TextComponent['fontFamily']) => {
+const buildFontWeight = (fontWeight: TextComponent["fontFamily"]) => {
   switch (fontWeight) {
-    case 'Thin':
-    case 'Thin_Italic':
+    case "Thin":
+    case "Thin_Italic":
       return 100;
-    case 'Light':
-    case 'Light_Italic':
+    case "Light":
+    case "Light_Italic":
       return 300;
-    case 'Regular':
-    case 'Regular_Italic':
+    case "Regular":
+    case "Regular_Italic":
       return 400;
-    case 'Bold':
-    case 'Bold_Italic':
+    case "Bold":
+    case "Bold_Italic":
       return 700;
-    case 'Black':
-    case 'Black_Italic':
+    case "Black":
+    case "Black_Italic":
       return 900;
   }
 };
 
-
-const Text = ({children, color = 'primary', fontFamily = 'Regular', fontSize = 16, ...rest}: TextComponent) => {
+const Text = ({
+  children,
+  color = "primary",
+  fontFamily = "Regular",
+  fontSize = 16,
+  ...rest
+}: TextComponent) => {
   const font = `Lato_${buildFontWeight(fontFamily) + fontFamily}`;
-  return <StyledText fontFamily={font} color={color} fontSize={fontSize} {...rest} >{children}</StyledText>;
+  return (
+    <StyledText fontFamily={font} color={color} fontSize={fontSize} {...rest}>
+      {children}
+    </StyledText>
+  );
 };
 
-
 interface TextComponent extends ColorProps, SpaceProps, FontSizeProps {
-  children: string
+  children: string;
   fontFamily?:
-    'Thin' |
-    'Thin_Italic' |
-    'Light' |
-    'Light_Italic' |
-    'Regular' |
-    'Regular_Italic' |
-    'Bold' |
-    'Bold_Italic' |
-    'Black' |
-    'Black_Italic'
+    | "Thin"
+    | "Thin_Italic"
+    | "Light"
+    | "Light_Italic"
+    | "Regular"
+    | "Regular_Italic"
+    | "Bold"
+    | "Bold_Italic"
+    | "Black"
+    | "Black_Italic";
 }
 
-
-export {Text};
+export { Text };
