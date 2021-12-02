dd64ed61654ad1f0e73cbbce22e96742b157caa9
Gabriel Couto <Gabriel_Couto@gap.com>
2021-12-01 22:03:11 -0300

    remove Header from shared/components

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,6 +2,5 @@ import { Text } from "./Text/TextComponent";
 import { View, SafeAreaView, AnimatedView } from "./View/ViewComponent";
 import { Pressable } from "./Pressable/PressableComponent";
 import { TextInput } from "./TextInput/TextInputComponent";
-import { Header } from "./Header/HeaderComponent";
 
-export { Text, View, SafeAreaView, Pressable, TextInput, AnimatedView, Header };
+export { Text, View, SafeAreaView, Pressable, TextInput, AnimatedView };
